3289bd85230294be99a24d6b3b0360ffb1aa2569
Kiverian Pinkard <kiverianpinkard@MacBook-Pro.home>
2021-02-01 17:10:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component}  from "react"; 
+import './App.css';
+import Info from './Info'
+class App extends Component {
+  constructor(props) {
+      // So that this.props is not undefined.
+      // Allows child class to inherit
+      super(props);
+      this.state = {
+          persons: [
+              {
+                  name: "Jamie",
+                  number: "(000) 000 0000",
+                  dob: "09/04/1998",
+                  hobby: "Coding",
+              },
+              {
+                  name: "Rick",
+                  number: "(000) 000 0000",
+                  dob: "05/04/1998",
+                  hobby: "Coding",
+              },
+              {
+                  name: "King",
+                  number: "(000) 000 0000",
+                  dob: "09/04/1999",
+                  hobby: "Coding",
+              },
+          ],
+      };
+  }
+  render() {
+      return (
+          <>
+              {this.state.persons.map((person) => {
+                  return (
+                      <Info
+                          key={person.number}
+                          name={person.name}
+                          number={person.number}
+                          dob={person.dob}
+                          hobby={person.hobby}
+                      />
+                  );
+              })}
+          </>
+      );
+  }
+}
+
+export default App;
